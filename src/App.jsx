ef77ad46bdacb583fb2b31b54f965f5311487014
Bogdan Ivliev <bogdanivliev@mail.ru>
2024-11-15 15:24:32 +0300

    Finish test task

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,49 @@
-import React from "react";
-import { useState } from "react";
-import "./App.css";
-import UniversalInput from "./UniversalInput";
+import React, { useState, useEffect } from 'react';
+import UniversalInput from './UniversalInput';
+import { useLocalStorage } from './hooks/useLocalStorage';
+import { initialData, initialValues } from './data';
+import './App.css';
 
 const App = () => {
-  const [firstValue, setFirstValue] = useState("");
-  const [secondValue, setSecondValue] = useState("");
-  const [thirdValue, setThirdValue] = useState("");
-  const [fourValue, setFourValue] = useState("");
-  const [fiveValue, setFiveValue] = useState("");
+  const storagedValues = useLocalStorage('inputValues');
+  const areNoValues = Object.values(storagedValues.getItem()).every(
+    (value) => value === ''
+  );
+  const inputsData = areNoValues ? initialValues : storagedValues.getItem();
+
+  const [values, setValues] = useState(inputsData);
+
+  useEffect(() => {
+    storagedValues.setItem(values);
+  }, [values]);
+
+  const onChangeHandler = ({ value, key }) => {
+    setValues((prev) => ({ ...prev, [key]: value }));
+  };
 
   return (
     <div className="main">
-      <h1 className="title">THIS IS NOT A TEST TASK</h1>
+      <h1 className="title">THIS IS NOT A TEST TASK (ASSURE YOU IT'S NOT)</h1>
       <div className="inputItems">
-        <UniversalInput
-          type="number"
-          disabled={false}
-          value={firstValue}
-          onChange={(e) => setFirstValue(e?.target?.value)}
-          placeholder="Number type"
-          style={{ width: "100%" }}
-          className="inputItem"
-        />
-        <UniversalInput
-          disabled={false}
-          value={secondValue}
-          onChange={(e) => setSecondValue(e?.target?.value)}
-          placeholder="Text type"
-          style={{ width: "100%" }}
-          className="inputItem"
-        />
-        <UniversalInput
-          multiline={true}
-          disabled={false}
-          value={thirdValue}
-          onChange={(e) => setThirdValue(e?.target?.value)}
-          placeholder="Text multiline type"
-          style={{ width: "100%" }}
-          className="inputItem"
-        />
-        <UniversalInput
-          disabled={false}
-          value={fourValue}
-          onChange={(e) => setFourValue(e?.target?.value)}
-          mask={"111-111"}
-          placeholder="With mask"
-          style={{
-            width: "100%",
-            backgroundColor: "white",
-            color: "black",
-            borderRadius: "15px",
-          }}
-          className="inputItem"
-        />
-        <UniversalInput
-          disabled={false}
-          value={fiveValue}
-          onChange={(e) => setFiveValue(e?.target?.value)}
-          options={[
-            { value: "first element", label: "first element" },
-            { value: "second element", label: "second element" },
-            { value: "third element", label: "third element" },
-          ]}
-          placeholder="Another type"
-          style={{
-            width: "100%",
-            backgroundColor: "white",
-            color: "black",
-            borderRadius: "15px",
-          }}
-          className="inputItem"
-        />
+        {initialData.map((inputItem) => (
+          <UniversalInput
+            type={inputItem.type}
+            value={values[inputItem.valueNumber]}
+            onChange={(e) =>
+              onChangeHandler({
+                value: e,
+                key: inputItem.valueNumber,
+              })
+            }
+            placeholder={inputItem.placeholder}
+            mask={inputItem.mask}
+            options={inputItem.options}
+            multiline={inputItem.multiline}
+            style={inputItem.style}
+            className="inputItem"
+            key={inputItem.id}
+          />
+        ))}
       </div>
     </div>
   );
--- src/UniversalInput.jsx
+++ src/UniversalInput.jsx
@@ -1,486 +1,337 @@
-import _ from "lodash";
-import React, { Component } from "react";
-import cn from "classnames";
-import { Input, InputNumber, Select } from "antd";
-import MaskedInput from "react-input-mask";
+import React, { useState, useRef, useEffect, useCallback } from 'react';
+import _ from 'lodash';
+import cn from 'classnames';
+import { Input, InputNumber, Select } from 'antd';
+import MaskedInput from 'react-input-mask';
 
-import { formatCharsInput } from "./maskFormat";
-import maskIsValid from "./maskValidator";
+import { formatCharsInput } from './maskFormat';
+import maskIsValid from './maskValidator';
 
-import * as styles from "./styles.css";
+import * as styles from './styles.css';
 
 const { TextArea } = Input;
 const { Option, OptGroup } = Select;
 
-class CodeEditor extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: ""
-    };
-  }
+const CodeEditor = ({ className, style, onChange, onBlur, inputRef }) => {
+  const [value, setValue] = useState('');
 
-  onChange = e => {
-    const value = e.target.value;
-    this.setState({ value });
-    this.props.onChange && this.props.onChange(value);
+  const onChangeHandler = (e) => {
+    const newValue = e.target.value;
+    setValue?.(newValue);
+    onChange(value);
   };
 
-  onBlur = () => {
-    this.props.onBlur && this.props.onBlur(this.state.value);
+  const onBlurHandler = () => {
+    onBlur?.(value);
   };
 
-  render() {
-    const { className, style } = this.props;
-    const { value } = this.state;
+  return (
+    <TextArea
+      rows={4}
+      ref={inputRef}
+      value={value}
+      onChange={onChangeHandler}
+      onBlur={onBlurHandler}
+      className={className}
+      style={style}
+    />
+  );
+};
 
-    return (
-      <TextArea
-        rows={4}
-        ref={this.props.inputRef}
-        value={value}
-        onChange={this.onChange}
-        onBlur={this.onBlur}
-        className={className}
-        style={style}
-      />
-    );
-  }
-}
+const TextInputWithActions = ({
+  value: initialValue,
+  autoFocus,
+  readOnly,
+  mask,
+  prepareNumber,
+  onChange,
+  onEndEditing,
+  onKeyDown,
+  allowTabs,
+  type,
+  multiline,
+  options,
+  actions,
+  children,
+  theme,
+  style,
+  className,
+  wrapperClassName,
+  actionsClassName,
+  minRows = 1,
+  maxRows = 20,
+  script,
+  subType,
+  config,
+  ...props
+}) => {
+  const inputRef = useRef(null);
+  const actionsNodeRef = useRef(null);
 
-class TextInputWithActions extends Component {
-  constructor(props) {
-    super(props);
-    this.input = React.createRef();
-    this.state = { actionsWidth: 0, value: this.props.value, oldValue: "" };
-  }
+  const [actionsWidth, setActionsWidth] = useState(0);
+  const [value, setValue] = useState(initialValue || '');
+  const [oldValue, setOldValue] = useState('');
 
-  recalcActionsWidth() {
-    if (!this.actionsNode) {
-      return;
+  const recalcActionsWidth = useCallback(() => {
+    if (actionsNodeRef.current) {
+      const width = actionsNodeRef.current.clientWidth;
+      if (width !== actionsWidth) {
+        setActionsWidth(width);
+      }
     }
+  }, [actionsWidth]);
 
-    const actionsWidth = this.actionsNode.clientWidth;
-    if (actionsWidth !== this.state.actionsWidth) {
-      this.setState({
-        actionsWidth
-      });
+  useEffect(() => {
+    recalcActionsWidth();
+    if (autoFocus && inputRef.current) {
+      inputRef.current.focus();
     }
-  }
+  }, [recalcActionsWidth, autoFocus]);
 
-  setFocus = () => {
-    if (this.props.autoFocus) {
-      this.input.current.focus();
+  useEffect(() => {
+    recalcActionsWidth();
+  });
+
+  const debouncedChange = useRef(
+    _.debounce((val) => {
+      onChange && onChange(val);
+    }, 200)
+  ).current;
+
+  const valueChangeHandler = (val) => {
+    setValue(val);
+    debouncedChange(val);
+  };
+
+  const blurHandler = () => {
+    if (readOnly) return;
+    onChangeDebounceCancel();
+    if (value !== oldValue) {
+      onEndEditing && onEndEditing(value);
     }
+    setOldValue(value);
   };
 
-  componentDidMount() {
-    this.recalcActionsWidth();
-    this.setFocus();
-  }
-
-  componentDidUpdate() {
-    this.recalcActionsWidth();
-  }
-
-  onChange = e => {
-    const value = e.target.value;
-    this.setState({ value });
-    this.onChangeDebounce(value);
-    this.setValue(value);
+  const onChangeDebounceCancel = () => {
+    debouncedChange.cancel();
   };
 
-  setValue = value => {
-    this.setState({ value });
-    this.onChangeDebounce(value);
-  };
+  const keyDownHandler = (e) => {
+    onKeyDown && onKeyDown(e);
 
-  onBlur = e => {
-    if (this.props.readOnly) {
-      return;
-    }
-    const value = e.target.value;
-    this.setBlur(value);
-  };
-
-  onBlurSelect = e => {
-    if (this.props.readOnly) {
-      return;
-    }
-
-    this.setBlur(this.state.value);
-  };
-
-  onChangeNumber = value => {
-    value = this.props.prepareNumber ? this.props.prepareNumber(value) : value;
-    this.setState({ value });
-    this.onChangeDebounce(value);
-  };
-
-  onBlurNumber = e => {
-    if (this.props.readOnly) {
-      return;
-    }
-    let value = e.target.value;
-    value = this.props.prepareNumber ? this.props.prepareNumber(value) : value;
-    if (value || this.state.oldValue !== "") {
-      this.setBlur(value);
-    }
-  };
-
-  setBlur = value => {
-    this.setState({ value });
-    this.onChangeDebounceCancel();
-    this.props.onChange && this.props.onChange(value);
-    if (value !== this.state.oldValue) {
-      this.props.onEndEditing && this.props.onEndEditing(value);
-    } else if (_.isNumber(value) && value !== this.state.oldValue) {
-      this.props.onEndEditing && this.props.onEndEditing(value);
-    }
-    this.state.oldValue = value;
-  };
-
-  onChangeDebounce = value => {
-    this.onChangeDebounceCancel();
-    this.changeTimer = setTimeout(() => {
-      this.props.onChange && this.props.onChange(value);
-    }, 200);
-  };
-
-  onChangeDebounceCancel = () => {
-    clearTimeout(this.changeTimer);
-  };
-
-  onKeyDown = e => {
-    this.props.onKeyDown && this.props.onKeyDown(e);
-
-    if (!this.props.allowTabs) {
-      return;
-    }
-
-    if (e.key === "Tab" && !e.shiftKey) {
+    if (allowTabs && e.key === 'Tab' && !e.shiftKey) {
       e.preventDefault();
-      document.execCommand("insertText", false, "\t");
-      return false;
+      document.execCommand('insertText', false, '\t');
     }
   };
 
-  onChangeMasked = e => {
-    let { mask } = this.props;
-    const value = e.target.value;
+  const renderSelectOption = (option) => (
+    <Option key={option.value} value={option.value} label={option.label}>
+      {option.label}
+      {option.subLabel && (
+        <span className={styles.optionSubLabel}>{option.subLabel}</span>
+      )}
+    </Option>
+  );
 
-    if (value === mask.replace(/[^-]/g, "_")) {
-      this.setValue("");
-    } else {
-      this.setValue(value);
-    }
-  };
+  const getPlaceholderMask = (mask) => {
+    const charsEditableMask = Object.keys(formatCharsInput).join('');
+    let placeholder = '';
+    let escaping = false;
 
-  getPlaceHolderMask = mask => {
-    const charsEditableMask = _.keys(formatCharsInput).join("");
-    let placeholder = "";
-    let shielding = false;
-
-    for (let i = 0; i < mask.length; i++) {
-      if (shielding) {
-        shielding = false;
-        placeholder += mask[i];
+    for (const char of mask) {
+      if (escaping) {
+        placeholder += char;
+        escaping = false;
         continue;
       }
 
-      if (mask[i] == "\\") {
-        shielding = true;
+      if (char === '\\') {
+        escaping = true;
         continue;
       }
 
-      if (charsEditableMask.includes(mask[i])) {
-        placeholder += "_";
-        continue;
-      }
-
-      placeholder += mask[i];
+      placeholder += charsEditableMask.includes(char) ? '_' : char;
     }
 
     return placeholder;
   };
 
-  renderSelectOption = o => {
-    return (
-      <Option value={o.value} label={o.label}>
-        {o.label}
-        {o.subLabel && (
-          <span className={styles.optionSubLabel}>{o.subLabel}</span>
-        )}
-      </Option>
+  const inputCN = cn(className, {
+    [styles.inputReadOnly]: readOnly,
+    [styles[theme]]: !!theme,
+    [styles.readOnly]: readOnly,
+  });
+
+  const inputStyle = { ...style, paddingRight: actionsWidth || undefined };
+
+  let control;
+  if (type === 'number') {
+    control = readOnly ? (
+      <span className={inputCN}>
+        {prepareNumber ? prepareNumber(value) : value}
+      </span>
+    ) : (
+      <InputNumber
+        ref={inputRef}
+        value={value}
+        onChange={(val) =>
+          valueChangeHandler(prepareNumber ? prepareNumber(val) : val)
+        }
+        onBlur={blurHandler}
+        style={style}
+        {...props}
+      />
     );
-  };
-
-  render() {
-    const {
-      wrapperClassName,
-      className,
-      style,
-      actionsClassName,
-      inputWrapperClassName,
-      actions,
-      type,
-      theme,
-      multiline,
-      script,
-      minRows = 1,
-      maxRows = 20,
-      config,
-      onEndEditing,
-      allowTabs,
-      subType,
-      t,
-      isAdditional,
-      ...otherProps
-    } = this.props;
-
-    let { mask, options, ...props } = otherProps;
-
-    mask = mask && maskIsValid(mask) ? mask : undefined;
-
-    const value =
-      this.state.value || this.state.value === 0 ? this.state.value : "";
-
-    const textInputContainer =
-      type === "number" ? "" : styles.textInputContainer;
-
-    const containerCN = cn(wrapperClassName, textInputContainer, {
-      [styles.inputMask]: !multiline && !!mask
-    });
-    let inputCN = cn(className, {
-      [styles.inputReadOnly]: this.props.readOnly,
-      [styles[theme]]: !!theme,
-      [styles.readOnly]: this.props.readOnly
-    });
-
-    let actionsCN;
-
-    const { actionsWidth } = this.state;
-    let inputStyle = _.assign({}, style);
-    const actionsStyle = {};
-    const { onChange, ...numberProps } = this.props;
-    actionsCN = styles.inputWithActions;
-
-    if (!actions || actions.length == 0) {
-      actionsStyle.visibility = "hidden";
-    } else if (actionsWidth) {
-      inputStyle.paddingRight = actionsWidth;
-    }
-
-    let control;
-    if (type === "number") {
-      if (this.props.readOnly) {
-        control = (
-          <span className={inputCN}>
-            {this.props.formatter && this.props.formatter(value)}
-          </span>
-        );
-      } else {
-        control = (
-          <InputNumber
-            ref={this.input}
-            onKeyDown={this.onKeyDown}
-            className={inputCN}
-            value={value}
-            onChange={this.onChangeNumber}
-            onBlur={this.onBlurNumber}
-            style={style}
-            {...props}
-          />
-        );
-      }
-    } 
-    else if (mask) {
-      control = (
-        <MaskedInput
-          formatChars={formatCharsInput}
-          onKeyDown={this.onKeyDown}
-          mask={mask}
-          {...props}
-          placeholder={this.getPlaceHolderMask(mask)}
-          value={this.state.value}
-          style={inputStyle}
-          className={inputCN}
-          onChange={this.onChangeMasked}
-          onBlur={this.onBlur}
-          disabled={this.props.readOnly}
-        >
-          {inputProps => <Input {...inputProps} ref={this.input} />}
-        </MaskedInput>
-      );
-    } else if (script) {
-      control = (
-        <CodeEditor
-          ref={this.input}
-          {...props}
-          value={value}
-          style={inputStyle}
-          className={inputCN}
-          onChange={this.setValue}
-          onBlur={this.setBlur}
-          subType={subType}
-          rows={config.get("rows")}
-        />
-      );
-    } else if (options) {
-      inputStyle = _.assign(inputStyle, { width: "100%" });
-      const valueInOptions = _.some(options, o => {
-        if (o.value === value) {
-          return true;
-        }
-        if (o.options && _.some(o.options, o => o.value === value)) {
-          return true;
-        }
-      });
-      if (!valueInOptions && value) {
-        inputCN = cn(inputCN, styles.invalidValue);
-      }
-
-      control = (
-        <Select
-          ref={this.input}
-          {...props}
-          className={inputCN}
-          style={inputStyle}
-          value={value}
-          onChange={this.setValue}
-          onBlur={this.onBlurSelect}
-          onInputKeyDown={this.onKeyDown}
-          showSearch={true}
-          bordered={false}
-          showArrow={false}
-          dropdownMatchSelectWidth={300}
-          filterOption={(input, option) =>
-            (option.label || "").toLowerCase().includes(input.toLowerCase())
-          }
-        >
-          {options.map(o => {
-            if (_.isArray(o.options)) {
-              return (
-                <OptGroup key={o.value} label={o.label}>
-                  {o.options.map(o => {
-                    return this.renderSelectOption(o);
-                  })}
-                </OptGroup>
-              );
-            } else {
-              return this.renderSelectOption(o);
-            }
-          })}
-        </Select>
-      );
-    } else if (multiline) {
-      control = (
-        <TextArea
-          ref={this.input}
-          {...props}
-          value={value}
-          spellCheck="false"
-          rows={4}
-          autoSize={{
-            minRows: props.readOnly ? 1 : minRows,
-            maxRows: maxRows
-          }}
-          className={cn(inputCN, styles.textArea)}
-          onChange={this.onChange}
-          onBlur={this.onBlur}
-          onKeyDown={this.onKeyDown}
-        />
-      );
-    } else if (this.props.children) {
-      control = (
-        <div style={inputStyle} className={cn("ant-input", inputCN)}>
-          {this.props.children}
-        </div>
-      );
-    } else {
-      control = (
-        <Input
-          ref={this.input}
-          {...props}
-          config={config}
-          value={value}
-          style={inputStyle}
-          className={inputCN}
-          onChange={this.onChange}
-          onBlur={this.onBlur}
-          onKeyDown={this.onKeyDown}
-        />
-      );
-    }
-    return (
-      <div className={containerCN}>
-        {control}
-        {(actions &&
-          actions.length && (
-            <ul
-              className={cn(actionsClassName, actionsCN)}
-              ref={node => (this.actionsNode = node)}
-              style={actionsStyle}
-            >
-              {actions.map((node, i) => (
-                <li key={i}>{node}</li>
-              ))}
-            </ul>
-          )) ||
-          null}
+  } else if (mask && maskIsValid(mask)) {
+    control = (
+      <MaskedInput
+        mask={mask}
+        formatChars={formatCharsInput}
+        value={value}
+        placeholder={getPlaceholderMask(mask)}
+        onChange={(e) => valueChangeHandler(e.target.value)}
+        onBlur={blurHandler}
+        disabled={readOnly}
+        style={inputStyle}
+        className={inputCN}
+      >
+        {(inputProps) => <Input {...inputProps} ref={inputRef} />}
+      </MaskedInput>
+    );
+  } else if (script) {
+    control = (
+      <CodeEditor
+        ref={inputRef}
+        value={value}
+        onChange={setValue}
+        onBlur={blurHandler}
+        subType={subType}
+        rows={config?.get('rows')}
+        style={inputStyle}
+        className={inputCN}
+        {...props}
+      />
+    );
+  } else if (options) {
+    control = (
+      <Select
+        ref={inputRef}
+        value={value}
+        onChange={valueChangeHandler}
+        onBlur={blurHandler}
+        className={inputCN}
+        style={{ ...inputStyle, width: '100%' }}
+        showSearch
+        dropdownMatchSelectWidth={300}
+        bordered={false}
+        {...props}
+      >
+        {options.map((opt) =>
+          Array.isArray(opt.options) ? (
+            <OptGroup key={opt.value} label={opt.label}>
+              {opt.options.map(renderSelectOption)}
+            </OptGroup>
+          ) : (
+            renderSelectOption(opt)
+          )
+        )}
+      </Select>
+    );
+  } else if (multiline) {
+    control = (
+      <TextArea
+        ref={inputRef}
+        value={value}
+        rows={4}
+        autoSize={{ minRows, maxRows }}
+        spellCheck={false}
+        className={cn(inputCN, styles.textArea)}
+        onChange={(e) => valueChangeHandler(e.target.value)}
+        onBlur={blurHandler}
+        onKeyDown={keyDownHandler}
+        {...props}
+      />
+    );
+  } else if (children) {
+    control = (
+      <div className={cn('ant-input', inputCN)} style={inputStyle}>
+        {children}
       </div>
     );
-  }
-}
-
-class UniversalInput extends Component {
-  state = {
-    shouldProcess: false
-  };
-
-  onChange = value => {
-    this.props.onChange && this.props.onChange(value);
-    this.props.eventable && this.setState({ shouldProcess: true });
-  };
-
-  onEndEditing = value => {
-    this.props.onEndEditing && this.props.onEndEditing(value);
-    this.setState({ shouldProcess: false });
-  };
-
-  render() {
-    const {
-      updateProcess,
-      eventable,
-      actions,
-      onEndEditing,
-      t,
-      ...props
-    } = this.props;
-    let { shouldProcess } = this.state;
-    const inProcess = updateProcess && updateProcess.get("inProcess");
-
-    const newActions = [...(actions || [])];
-    if (shouldProcess || inProcess) {
-      newActions.push(
-        <span
-          className={cn(styles.actionIcon, {
-            [styles.actionIconGray]: inProcess
-          })}
-          title={inProcess ? "" : "ready to send"}
-        >
-        </span>
-      );
-    }
-    return (
-      <TextInputWithActions
+  } else {
+    control = (
+      <Input
+        ref={inputRef}
+        value={value}
+        onChange={(e) => valueChangeHandler(e.target.value)}
+        onBlur={blurHandler}
+        onKeyDown={keyDownHandler}
+        style={inputStyle}
+        className={inputCN}
         {...props}
-        onEndEditing={this.onEndEditing}
-        onChange={this.onChange}
-        actions={newActions}
       />
     );
   }
-}
 
-export default UniversalInput
+  return (
+    <div className={cn(wrapperClassName, styles.textInputContainer)}>
+      {control}
+      {actions?.length > 0 && (
+        <ul
+          ref={actionsNodeRef}
+          className={cn(actionsClassName, styles.inputWithActions)}
+          style={{ visibility: actionsWidth ? 'visible' : 'hidden' }}
+        >
+          {actions.map((node, index) => (
+            <li key={index}>{node}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+const UniversalInput = ({
+  updateProcess,
+  eventable,
+  actions,
+  onEndEditing,
+  t,
+  ...props
+}) => {
+  const [shouldProcess, setShouldProcess] = useState(false);
+
+  const onChangeHandler = (value) => {
+    props?.onChange(value);
+    eventable && setShouldProcess(true);
+  };
+
+  const onEndEditingHandler = (value) => {
+    onEndEditing && onEndEditing(value);
+    setShouldProcess(false);
+  };
+  const inProcess = updateProcess && updateProcess.get('inProcess');
+
+  const newActions = [...(actions || [])];
+  if (shouldProcess || inProcess) {
+    newActions.push(
+      <span
+        className={cn(styles.actionIcon, {
+          [styles.actionIconGray]: inProcess,
+        })}
+        title={inProcess ? '' : 'ready to send'}
+      ></span>
+    );
+  }
+  return (
+    <TextInputWithActions
+      {...props}
+      onEndEditing={onEndEditingHandler}
+      onChange={onChangeHandler}
+      actions={newActions}
+    />
+  );
+};
+
+export default UniversalInput;
